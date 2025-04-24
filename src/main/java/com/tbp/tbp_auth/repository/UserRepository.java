f1e1db2fe412b1784dac9cb085bf3545f165d55f
Lobo <andreas.dahm@gmail.com>
2025-04-24 09:51:28 +0200

    Added new endpoint to register new users

---
+++ src/main/java/com/tbp/tbp_auth/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.tbp.tbp_auth.repository;
+
+import com.tbp.tbp_auth.datamodel.dao.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
