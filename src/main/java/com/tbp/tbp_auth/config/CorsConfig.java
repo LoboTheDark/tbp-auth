e172ca3692754a4ec675ea3a6ab6681e9fc76937
Lobo <andreas.dahm@gmail.com>
2025-04-27 10:12:25 +0200

    Added cors for local dev

---
+++ src/main/java/com/tbp/tbp_auth/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.tbp.tbp_auth.config;
+
+import jakarta.annotation.Nullable;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Slf4j
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nullable  CorsRegistry registry) {
+                if(registry == null)
+                {
+                    log.warn("Could not configure CORS, because registry is null.");
+                    return;
+                }
+
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
