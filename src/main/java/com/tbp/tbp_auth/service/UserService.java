15d9c4a86d1cc0b0f271acde2d1762bb2c4a6d13
Lobo <andreas.dahm@gmail.com>
2025-05-17 13:15:12 +0200

    Added new data and callbnacks

--- src/main/java/com/tbp/tbp_auth/service/UserService.java
+++ src/main/java/com/tbp/tbp_auth/service/UserService.java
@@ -6,9 +6,15 @@ import com.tbp.tbp_auth.dto.requests.RegisterUserRequest;
 import com.tbp.tbp_auth.dto.responses.UserResponseDto;
 import com.tbp.tbp_auth.exception.UserAlreadyExistsException;
 import com.tbp.tbp_auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.UUID;
 
 @Service
@@ -17,7 +23,10 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    @Value("${backend.syncSteamApiUrl}")
+    private String backendSyncUrl;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
@@ -26,6 +35,8 @@ public class UserService {
         duplicateCheck(request);
         var user = createNewUserObject(request);
         var savedUser = userRepository.save(user);
+
+        callBackendToSync(savedUser.getSteamId());
 
         return new UserResponseDto(
                 savedUser.getId(),
@@ -36,6 +47,23 @@ public class UserService {
                 savedUser.getProvider()
         );
     }
+
+    private void callBackendToSync(String steamId) {
+
+
+        try (var client = HttpClient.newHttpClient()) {
+            var request = HttpRequest.newBuilder()
+                    .uri(URI.create(backendSyncUrl))
+                    .header("Content-Type", "application/json")
+                    .timeout(Duration.ofMinutes(1))
+                    .POST(HttpRequest.BodyPublishers.ofString(steamId))
+                    .build();
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .join();
+        }
+    }
+
 
     private User createNewUserObject(RegisterUserRequest request) {
         var user = new User();
